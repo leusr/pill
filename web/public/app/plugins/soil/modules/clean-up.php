b2cd3ed1bbb5828e23f7c1dce034aec38ad891f6
G2P <gyuripp@gmail.com>
2017-03-24 23:36:18 +0100

    Fix head clean up

--- web/public/app/plugins/soil/modules/clean-up.php
+++ web/public/app/plugins/soil/modules/clean-up.php
@@ -24,22 +24,23 @@ function head_cleanup() {
 	// Originally from http://wpengineer.com/1438/wordpress-header/
 	remove_action( 'wp_head', 'feed_links_extra', 3 );
 	add_action( 'wp_head', 'ob_start', 1, 0 );
-	add_action( 'wp_head', function() {
-		$pattern = '/.*' . preg_quote( esc_url( get_feed_link( 'comments_' . get_default_feed() ) ), '/' ) . '.*[\r\n]+/';
+	add_action( 'wp_head', function () {
+		$pattern = '/.*' . preg_quote( esc_url( get_feed_link( 'comments_' . get_default_feed() ) ), '/' )
+		           . '.*[\r\n]+/';
 		echo preg_replace( $pattern, '', ob_get_clean() );
 	}, 3, 0 );
 	remove_action( 'wp_head', 'rsd_link' );
 	remove_action( 'wp_head', 'wlwmanifest_link' );
-	remove_action( 'wp_head', 'adjacent_posts_rel_link_wp_head', 10, 0 );
+	remove_action( 'wp_head', 'adjacent_posts_rel_link_wp_head', 10 );
 	remove_action( 'wp_head', 'wp_generator' );
-	remove_action( 'wp_head', 'wp_shortlink_wp_head', 10, 0 );
+	remove_action( 'wp_head', 'wp_shortlink_wp_head', 10 );
+	remove_action( 'wp_head', 'print_emoji_detection_script', 7 );
+	remove_action( 'admin_print_scripts', 'print_emoji_detection_script' );
+	remove_action( 'wp_print_styles', 'print_emoji_styles' );
+	remove_action( 'admin_print_styles', 'print_emoji_styles' );
 	remove_action( 'wp_head', 'wp_oembed_add_discovery_links' );
 	remove_action( 'wp_head', 'wp_oembed_add_host_js' );
-	remove_action( 'wp_head', 'rest_output_link_wp_head', 10, 0 );
-	remove_action( 'wp_head', 'print_emoji_detection_script' );
-	remove_action( 'admin_print_scripts', 'print_emoji_detection_script' );
-	remove_action( 'admin_print_styles', 'print_emoji_styles' );
-	remove_action( 'wp_print_styles', 'print_emoji_styles' );
+	remove_action( 'wp_head', 'rest_output_link_wp_head', 10 );
 	remove_filter( 'the_content_feed', 'wp_staticize_emoji' );
 	remove_filter( 'comment_text_rss', 'wp_staticize_emoji' );
 	remove_filter( 'wp_mail', 'wp_staticize_emoji_for_email' );
@@ -49,10 +50,8 @@ function head_cleanup() {
 	global $wp_widget_factory;
 
 	if ( isset( $wp_widget_factory->widgets['WP_Widget_Recent_Comments'] ) ) {
-		remove_action( 'wp_head', [
-			$wp_widget_factory->widgets['WP_Widget_Recent_Comments'],
-			'recent_comments_style'
-		] );
+		remove_action( 'wp_head',
+		               [ $wp_widget_factory->widgets['WP_Widget_Recent_Comments'], 'recent_comments_style' ] );
 	}
 
 	if ( ! class_exists( 'WPSEO_Frontend' ) ) {
@@ -60,6 +59,7 @@ function head_cleanup() {
 		add_action( 'wp_head', __NAMESPACE__ . '\\rel_canonical' );
 	}
 }
+
 add_action( 'init', __NAMESPACE__ . '\\head_cleanup' );
 
 /**
@@ -107,32 +107,36 @@ function language_attributes() {
 
 	return $output;
 }
+
 add_filter( 'language_attributes', __NAMESPACE__ . '\\language_attributes' );
 
 /**
  * Clean up output of stylesheet <link> tags
  *
  * @param string $input
+ *
  * @return string
  */
 function clean_style_tag( $input ) {
-    $link = "!<link rel='stylesheet'\s?(id='[^']+')?\s+href='(.*)' type='text/css' media='(.*)' />!";
+	$link = "!<link rel='stylesheet'\s?(id='[^']+')?\s+href='(.*)' type='text/css' media='(.*)' />!";
 	preg_match_all( $link, $input, $matches );
-    if ( empty( $matches[2] ) ) {
-        return $input;
-    }
+	if ( empty( $matches[2] ) ) {
+		return $input;
+	}
 
 	// Only display media if it is meaningful
 	$media = $matches[3][0] !== '' && $matches[3][0] !== 'all' ? ' media="' . $matches[3][0] . '"' : '';
 
 	return '<link rel="stylesheet" href="' . $matches[2][0] . '"' . $media . '>' . "\n";
 }
+
 add_filter( 'style_loader_tag', __NAMESPACE__ . '\\clean_style_tag' );
 
 /**
  * Clean up output of <script> tags
  *
  * @param string $input
+ *
  * @return mixed
  */
 function clean_script_tag( $input ) {
@@ -140,6 +144,7 @@ function clean_script_tag( $input ) {
 
 	return str_replace( "'", '"', $input );
 }
+
 add_filter( 'script_loader_tag', __NAMESPACE__ . '\\clean_script_tag' );
 
 /**
@@ -148,11 +153,13 @@ add_filter( 'script_loader_tag', __NAMESPACE__ . '\\clean_script_tag' );
  * @link http://www.readability.com/publishers/guidelines#publisher
  *
  * @param string $cache
+ *
  * @return string
  */
 function embed_wrap( $cache ) {
 	return '<div class="entry-content-asset">' . $cache . '</div>';
 }
+
 add_filter( 'embed_oembed_html', __NAMESPACE__ . '\\embed_wrap' );
 
 /**
@@ -165,17 +172,20 @@ function remove_dashboard_widgets() {
 	// remove_meta_box( 'dashboard_primary', 'dashboard', 'normal' );
 	remove_meta_box( 'dashboard_secondary', 'dashboard', 'normal' );
 }
+
 add_action( 'admin_init', __NAMESPACE__ . '\\remove_dashboard_widgets' );
 
 /**
  * Remove unnecessary self-closing tags
  *
  * @param string $input
+ *
  * @return string
  */
 function remove_self_closing_tags( $input ) {
 	return str_replace( ' />', '>', $input );
 }
+
 add_filter( 'get_avatar', __NAMESPACE__ . '\\remove_self_closing_tags' ); // <img />
 add_filter( 'comment_id_fields', __NAMESPACE__ . '\\remove_self_closing_tags' ); // <input />
 add_filter( 'post_thumbnail_html', __NAMESPACE__ . '\\remove_self_closing_tags' ); // <img />
@@ -184,6 +194,7 @@ add_filter( 'post_thumbnail_html', __NAMESPACE__ . '\\remove_self_closing_tags' 
  * Don't return the default description in the RSS feed if it hasn't been changed
  *
  * @param string $bloginfo
+ *
  * @return string
  */
 function remove_default_description( $bloginfo ) {
@@ -191,12 +202,14 @@ function remove_default_description( $bloginfo ) {
 
 	return ( $bloginfo === $default_tagline ) ? '' : $bloginfo;
 }
+
 add_filter( 'get_bloginfo_rss', __NAMESPACE__ . '\\remove_default_description' );
 
 /**
  * Filter function used to remove the tinymce emoji plugin.
  *
  * @param array $plugins
+ *
  * @return array Difference betwen the two arrays
  */
 function disable_emojis_tinymce( $plugins ) {
@@ -206,13 +219,14 @@ function disable_emojis_tinymce( $plugins ) {
 		return [];
 	}
 }
+
 add_filter( 'tiny_mce_plugins', __NAMESPACE__ . '\\disable_emojis_tinymce' );
 
 /**
  * Unregister WP Widgets to speeding up WP Admin.
  */
 function unregister_wp_widgets() {
-	$widgets = array(
+	$widgets = [
 		'WP_Widget_Pages',
 		'WP_Widget_Calendar',
 		'WP_Widget_Archives',
@@ -226,11 +240,12 @@ function unregister_wp_widgets() {
 		'WP_Widget_Recent_Comments',
 		'WP_Widget_RSS',
 		'WP_Widget_Tag_Cloud',
-		'WP_Nav_Menu_Widget'
-	);
+		'WP_Nav_Menu_Widget',
+	];
 
-	foreach ($widgets as $widget ) {
+	foreach ( $widgets as $widget ) {
 		unregister_widget( $widget );
 	}
 }
+
 add_action( 'widgets_init', __NAMESPACE__ . '\\unregister_wp_widgets' );
