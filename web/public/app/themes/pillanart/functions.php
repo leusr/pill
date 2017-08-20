47479ef51b45c9222bf00d3606a77d49721113fb
György Papp <gyorgy.papp@hbo.com>
2017-08-20 16:21:57 +0200

    Add Facebook Pixel

--- web/public/app/themes/pillanart/functions.php
+++ web/public/app/themes/pillanart/functions.php
@@ -75,6 +75,9 @@ class Pillanart {
 
 		// Support editor style
 		add_editor_style();
+
+        // Add Facebook Pixel
+        add_action( 'wp_head', [ $this, 'facebook_pixel' ], 10, 0 );
 	}
 
 	/**
@@ -91,6 +94,22 @@ class Pillanart {
 				'qsxga'         => __( 'QSXGA', 'pillanart-theme' )
 		] );
 	}
+
+    /**
+     * Insert Facebook Pixel for advertising
+     */
+    public function facebook_pixel() { ?>
+
+        <!-- Facebook Pixel Code -->
+        <script>!function(f,b,e,v,n,t,s){if(f.fbq)return;n=f.fbq=function(){n.callMethod?n.callMethod.apply(n,arguments):n.queue.push(arguments)};if(!f._fbq)f._fbq=n;
+            n.push=n;n.loaded=!0;n.version='2.0';n.queue=[];t=b.createElement(e);t.async=!0;t.src=v;s=b.getElementsByTagName(e)[0];s.parentNode.insertBefore(t,s)
+          }(window,document,'script','https://connect.facebook.net/en_US/fbevents.js');
+          fbq('init', '231918430666546');fbq('track', 'PageView');</script>
+        <noscript><img height="1" width="1" style="display:none" src="https://www.facebook.com/tr?id=231918430666546&ev=PageView&noscript=1"></noscript>
+        <!-- End Facebook Pixel Code -->
+
+    <?php
+    }
 
 	/**
 	 * Customize the excerpt
